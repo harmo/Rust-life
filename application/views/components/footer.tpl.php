7a221351637078639638402ec6463ba9a3f75498
harmo <dtharmo@gmail.com>
2014-05-24 12:27:45 +0200

    Deleted old template

--- application/views/components/footer.tpl.php
+++
@@ -1,5 +0,0 @@
-    <script type="text/javascript" src="<?php echo $static->js('jQuery-2.1.1/jQuery-2.1.1.min', 'vendor'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $static->js('jQuery-ui/js/jquery-ui-1.10.4.custom.min', 'vendor'); ?>"></script>
-    <script type="text/javascript" src="<?php echo $static->js('bootstrap-3.1.1/js/bootstrap.min', 'vendor'); ?>"></script>
-</body>
-</html>
