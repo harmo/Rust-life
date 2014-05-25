285132229b1d28b97f4d2291a45da2a0c97dd6f8
harmo <dtharmo@gmail.com>
2014-05-25 13:47:08 +0200

    Added home jumbotron

--- application/views/main.php
+++ application/views/main.php
@@ -1,1 +1,9 @@
-MAIN
+<?php if(!isset($user) || !$user): ?>
+    <div class="jumbotron text-center">
+        <h1>Rejoignez-nous!</h1>
+        <p class="lead">
+            Une chaleureuse communautée en constante évolution avec déjà 198 joueurs inscrits !
+        </p>
+        <a class="btn btn-lg btn-success" href="register">Inscription</a>
+    </div>
+<?php endif; ?>
